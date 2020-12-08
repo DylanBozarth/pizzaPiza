812893126849e7e743ff2fade1511c106e55f672
dylan <dylanabozarth@gmail.com>
2020-12-07 20:40:44 -0800

    skelliton

--- src/CYO.js
+++ src/CYO.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 const CYO = () => {
+    const [pizzaSize, setPizzaSize] = useState(null);
+const changePizza = () => {
+setPizzaSize()
+}
 return (
+
+    
     <div className="container">
         <h1 className="text-center">Create your own pizza</h1><div className="row">
             
 <div className="col-sm-8">
-<img src="./download.jpeg" className="img-fluid"></img>
+<p>{pizzaSize}</p>
 </div>
 <div className="col-sm-4">
-Menu aaa
+    <h3>Pizza size</h3>
+    <button type="button" onClick={() => changePizza()}>Medium</button><button type="button">Large</button> 
 </div>
 
 </div>
