00c1fd166535905e579d11396b22d34ae6c0562c
dylan <dylanabozarth@gmail.com>
2020-12-09 16:15:26 -0800

    added meats

--- src/CYO.js
+++ src/CYO.js
@@ -1,28 +1,24 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const CYO = () => {
   const [pizzaSize, setPizzaSize] = useState(null);
   const [toppings, setToppings] = useState([]);
- 
+
   const [startPrice, setStartPrice] = useState(0);
   // const [toppingPrice, setToppingPrice] = useState(0)
   const changePizza = (e) => {
     setPizzaSize(e.target.name);
-    setStartPrice(parseInt(e.target.value))
-    
+    setStartPrice(parseInt(e.target.value));
   };
   const changeToppings = (e) => {
     setToppings([...toppings, e.target.value]);
   };
-const countToppings = () => {
-  
-  console.log('aa')
+  const countToppings = () => {
+    console.log("aa");
+  };
+  let toppingPrice = toppings.length * 1.5;
+  let price = startPrice + toppingPrice;
 
-
-}
-let toppingPrice = toppings.length * 1.5
-let price = startPrice + toppingPrice
-  
   return (
     <div className="container">
       <h1 className="text-center">Create your own pizza</h1>
@@ -37,8 +33,8 @@ let price = startPrice + toppingPrice
             type="button"
             value="10"
             name="Medium"
-            onClick={(event) => {changePizza(event)
-            
+            onClick={(event) => {
+              changePizza(event);
             }}
           >
             Medium
@@ -47,7 +43,8 @@ let price = startPrice + toppingPrice
             type="button"
             value="12"
             name="Large"
-            onClick={(event) => {changePizza(event)
+            onClick={(event) => {
+              changePizza(event);
             }}
           >
             Large
@@ -56,7 +53,8 @@ let price = startPrice + toppingPrice
             type="button"
             value="15"
             name="Large"
-            onClick={(event) => {changePizza(event)
+            onClick={(event) => {
+              changePizza(event);
             }}
           >
             Extra Large
@@ -65,18 +63,51 @@ let price = startPrice + toppingPrice
           <button
             type="button"
             value="Peperoni "
-            onClick={(event) => {changeToppings(event) 
-            countToppings()}}
+            onClick={(event) => {
+              changeToppings(event);
+              countToppings();
+            }}
           >
             Pepperoni
           </button>
-          
-          
-          
-          
-          
-          
-          
+          <button
+            type="button"
+            value="Sausage"
+            onClick={(event) => {
+              changeToppings(event);
+              countToppings();
+            }}
+          >
+            Sausage
+          </button>
+          <button
+            type="button"
+            value="Beef"
+            onClick={(event) => {
+              changeToppings(event);
+              countToppings();
+            }}
+          >
+            Beef
+          </button>
+          <button
+            type="button"
+            value="Salami"
+            onClick={(event) => {
+              changeToppings(event);
+              countToppings();
+            }}
+          >
+            Salami
+          </button>
+          <button
+            type="button"
+            value="Bacon"
+            onClick={(event) => {changeToppings(event) 
+            countToppings()}}
+          >
+            Bacon
+          </button>
           <p>Price: {price}</p>
         </div>
       </div>
