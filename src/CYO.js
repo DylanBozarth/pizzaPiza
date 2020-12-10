1c6974a97d61db4685b398055d25db6b9820c67a
dylan <dylanabozarth@gmail.com>
2020-12-09 22:11:42 -0800

    styling

--- src/CYO.js
+++ src/CYO.js
@@ -1,8 +1,8 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 const CYO = () => {
-  const [pizzaSize, setPizzaSize] = useState('Choose your Pizza Size');
-  
+  const [pizzaSize, setPizzaSize] = useState("Choose your Pizza Size");
+
   const [toppings, setToppings] = useState([]);
 
   const [startPrice, setStartPrice] = useState(0);
@@ -14,19 +14,27 @@ const CYO = () => {
   const changeToppings = (e) => {
     setToppings([...toppings, e.target.value]);
   };
- 
+
   let toppingPrice = toppings.length * 1.5;
   let price = startPrice + toppingPrice;
 
   return (
-    <div className="container">
-      <h1 className="text-center">Create your own pizza</h1>
+    <div className="container CYO">
+      <h2 className="text-center">Create your own pizza</h2>
       <div className="row">
         <div className="col-sm-8">
-          <p>{pizzaSize}</p>
-          <p>Price: ${price}</p>
-          <img src="./pizza.png" className="img-fluid pizza" alt="Pizza"></img>
-          <p>Your Toppings:  <br />{toppings}</p>
+          <div className="">
+            <img
+              src="./pizza.png"
+              className="img-fluid pizza"
+              alt="Pizza"
+            ></img>
+          </div>
+          <h3>{pizzaSize}</h3>
+          <p>
+            Your Toppings: <br />
+            {toppings}
+          </p>
         </div>
         <div className="col-sm-4">
           <h3>Pizza size</h3>
@@ -62,14 +70,13 @@ const CYO = () => {
           </button>
           <br />
           <h3>Toppings</h3>
-          
+
           <h4>Meats</h4>
           <button
             type="button"
             value="Peperoni "
             onClick={(event) => {
               changeToppings(event);
-              
             }}
           >
             Pepperoni
@@ -79,7 +86,6 @@ const CYO = () => {
             value="Sausage "
             onClick={(event) => {
               changeToppings(event);
-              
             }}
           >
             Sausage
@@ -89,7 +95,6 @@ const CYO = () => {
             value="Beef "
             onClick={(event) => {
               changeToppings(event);
-             
             }}
           >
             Beef
@@ -99,7 +104,6 @@ const CYO = () => {
             value="Salami "
             onClick={(event) => {
               changeToppings(event);
-              
             }}
           >
             Salami
@@ -107,8 +111,9 @@ const CYO = () => {
           <button
             type="button"
             value="Bacon "
-            onClick={(event) => {changeToppings(event) 
-           }}
+            onClick={(event) => {
+              changeToppings(event);
+            }}
           >
             Bacon
           </button>
@@ -116,19 +121,26 @@ const CYO = () => {
           <button
             type="button"
             value="Green Peppers "
-            onClick={(event) => {changeToppings(event) 
-           }}
+            onClick={(event) => {
+              changeToppings(event);
+            }}
           >
             Green Peppers
           </button>
           <button
             type="button"
             value="Onions "
-            onClick={(event) => {changeToppings(event) 
-           }}
+            onClick={(event) => {
+              changeToppings(event);
+            }}
           >
             Onions
           </button>
+        </div>
+
+        <div className="pricefooter">
+          <p>Price: ${price}</p>
+
         </div>
       </div>
     </div>
