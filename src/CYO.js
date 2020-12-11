965ba624e4165d00abe081d3b0a04cd26880acf9
dylan <dylanabozarth@gmail.com>
2020-12-10 19:13:25 -0800

    before styling

--- src/CYO.js
+++ src/CYO.js
@@ -14,7 +14,17 @@ const CYO = () => {
   const changeToppings = (e) => {
     setToppings([...toppings, e.target.value]);
   };
-
+const CheckSize = () => {
+  if (pizzaSize === "Choose your Pizza Size") {
+    alert('You must choose a pizza size')
+  }
+  if (toppings.length === 0 ) {
+    alert ("Are you sure you don't want toppings?")
+  }
+  else {
+    alert("Sorry, this isn't a real pizza place.")
+  }
+}
   let toppingPrice = toppings.length * 1.5;
   let price = startPrice + toppingPrice;
 
@@ -74,7 +84,7 @@ const CYO = () => {
           <h4>Meats</h4>
           <button
             type="button"
-            value="Peperoni "
+            value="Peperoni, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -83,7 +93,7 @@ const CYO = () => {
           </button>
           <button
             type="button"
-            value="Sausage "
+            value="Sausage, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -92,7 +102,7 @@ const CYO = () => {
           </button>
           <button
             type="button"
-            value="Beef "
+            value="Beef, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -101,7 +111,7 @@ const CYO = () => {
           </button>
           <button
             type="button"
-            value="Salami "
+            value="Salami, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -110,7 +120,7 @@ const CYO = () => {
           </button>
           <button
             type="button"
-            value="Bacon "
+            value="Bacon, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -120,7 +130,7 @@ const CYO = () => {
           <h4>Veggies</h4>
           <button
             type="button"
-            value="Green Peppers "
+            value="Green Peppers, "
             onClick={(event) => {
               changeToppings(event);
             }}
@@ -129,13 +139,41 @@ const CYO = () => {
           </button>
           <button
             type="button"
-            value="Onions "
+            value="Onions, "
             onClick={(event) => {
               changeToppings(event);
             }}
           >
             Onions
           </button>
+          <button
+            type="button"
+            value="Tomatoes, "
+            onClick={(event) => {
+              changeToppings(event);
+            }}
+          >
+            Tomatoes
+          </button>
+          <button
+            type="button"
+            value="Artichokes, "
+            onClick={(event) => {
+              changeToppings(event);
+            }}
+          >
+           Artichokes
+          </button>
+          <button
+            type="button"
+            value="Mushrooms, "
+            onClick={(event) => {
+              changeToppings(event);
+            }}
+          >
+            Mushrooms
+          </button>
+          <br /><button className="checkout" onClick={CheckSize}>Checkout</button>
         </div>
 
         <div className="pricefooter">
